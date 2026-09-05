047848c0a0a3c051ea1f436b5a913d443d0e751c
SARHAN SHERIFF <as8510@srmist.edu.in>
2026-09-05 09:21:24 +0530

    feat(session-2): add masked phone class problem

---
+++ src/main/java/strings/class_problems/MaskedPhoneNumberFormatter.java
@@ -0,0 +1,2 @@
+package strings.class_problems;
+public class MaskedPhoneNumberFormatter { static String maskPhoneNumber(String phone){if(phone==null||phone.length()!=10)return"Invalid phone number";for(char c:phone.toCharArray())if(!Character.isDigit(c))return"Invalid phone number";StringBuilder b=new StringBuilder("XXXXXX");b.insert(6,"-");b.append(phone.substring(6));return b.toString();} public static void main(String[]a){System.out.println(maskPhoneNumber("9876543210"));}}
