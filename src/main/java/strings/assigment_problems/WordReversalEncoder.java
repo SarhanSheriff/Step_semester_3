18173b6411cc19d618b48f0b5ab49456ea4f56c1
SARHAN SHERIFF <as8510@srmist.edu.in>
2026-09-05 09:21:05 +0530

    feat(session-2): add word reversal assignment

---
+++ src/main/java/strings/assigment_problems/WordReversalEncoder.java
@@ -0,0 +1,2 @@
+package strings.assigment_problems;
+public class WordReversalEncoder { static String reverseEachWord(String sentence){String[] words=sentence.split(" ");StringBuilder out=new StringBuilder();for(int i=0;i<words.length;i++){if(i>0)out.append(' ');out.append(new StringBuilder(words[i]).reverse());}return out.toString();} public static void main(String[]a){System.out.println(reverseEachWord("hello club"));}}
