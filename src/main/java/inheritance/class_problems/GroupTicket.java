10360ed00dafdf4fd7d9842aad48c8e5813e67fc
SARHAN SHERIFF <as8510@srmist.edu.in>
2026-09-12 11:10:29 +0530

    refactor(session-6): shorten GroupTicket

--- src/main/java/inheritance/class_problems/GroupTicket.java
+++ src/main/java/inheritance/class_problems/GroupTicket.java
@@ -3,11 +3,10 @@ package inheritance.class_problems;
 public class GroupTicket extends EventTicket {
     private final int groupSize;
 
-    public GroupTicket(double basePrice, int groupSize) {
-        super(basePrice);
-        if (groupSize <= 0) {
+    public GroupTicket(double price, int groupSize) {
+        super(price);
+        if (groupSize <= 0)
             throw new IllegalArgumentException("Group size must be positive");
-        }
         this.groupSize = groupSize;
     }
 
