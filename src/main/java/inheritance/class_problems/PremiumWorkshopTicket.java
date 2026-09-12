7af3ff1afc693bd68b1c2463024afe6ffca2d499
SARHAN SHERIFF <as8510@srmist.edu.in>
2026-09-12 10:58:34 +0530

    feat(session-6): add PremiumWorkshopTicket multilevel inheritance

---
+++ src/main/java/inheritance/class_problems/PremiumWorkshopTicket.java
@@ -0,0 +1,33 @@
+package inheritance.class_problems;
+
+public class PremiumWorkshopTicket extends WorkshopTicket {
+    private final double kitFee;
+
+    public PremiumWorkshopTicket(
+            String attendeeId,
+            double basePrice,
+            String track,
+            double kitFee) {
+        super(attendeeId, basePrice, track);
+        if (kitFee < 0) {
+            throw new IllegalArgumentException("Kit fee cannot be negative");
+        }
+        this.kitFee = kitFee;
+    }
+
+    public double getKitFee() {
+        return kitFee;
+    }
+
+    @Override
+    public double getBalanceDue() {
+        return super.getBalanceDue() + kitFee;
+    }
+
+    @Override
+    public void printTicket() {
+        System.out.println("Premium Workshop Ticket | Track: " + getTrack()
+                + " | Kit Fee: " + kitFee
+                + " | Balance Due: " + getBalanceDue());
+    }
+}
